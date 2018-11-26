6d46e6e38c23fdba4e66645623f7e36c4520f747
Mandl Cho <mandl.cho@icloud.com>
2018-11-26 17:49:05 +0800

    Removed dependency on pyfbsdk
    converting this to a full pythonic script instead of having it work in motionbuilder only.

--- pyfbsdk_wheretoeat.py
+++ pyfbsdk_wheretoeat.py
@@ -1,11 +1,20 @@
-import pyfbsdk as fb
-import pyfbsdk_additions as fba
+# import pyfbsdk as fb
+# import pyfbsdk_additions as fba
+
+# import sys
+# from PyQt4 import QtGui
 
 import random as rand
 
-# define buttons
-btnGenerous = fb.FBButton()
-btnBroke = fb.FBButton()
+# app = QtGui.QApplication(sys.argv)
+
+# window = QtGui.QWidget()
+# window.setGeometry(0,0,500,300)
+# window.setWindowTitle("Where To Eat!")
+
+# # define buttons
+# btnGenerous = fb.FBButton()
+# btnBroke = fb.FBButton()
 
 lFoodLocations = ["DailyCut",
                   "Subway", 
@@ -30,7 +39,8 @@ def WhereToEat(pBudgetValue):
         cheapPlaceRandomIdx = rand.randint(0,cheapnum)
         cheapLocation = lCheapFoodLocations[cheapPlaceRandomIdx]
         # places that cost 10 or below
-        fb.FBMessageBox("Eat Cheap la", cheapLocation, "lets go")
+        # fb.FBMessageBox("Eat Cheap la", cheapLocation, "lets go")
+        print cheapLocation
 
     if pBudgetValue >= 11:
         # places that cost 11 or more
@@ -38,8 +48,10 @@ def WhereToEat(pBudgetValue):
         exnum = len(lExListCount)
         exPlaceRandomIdx = rand.randint(0,exnum)
         exLocation = lFoodLocations[exPlaceRandomIdx]
-        fb.FBMessageBox("Feeling Generous eh?", exLocation, "lets go")
+        print exLocation
+        # fb.FBMessageBox("Feeling Generous eh?", exLocation, "lets go")
 
         # pass
 
 WhereToEat(5)
+# window.show()
