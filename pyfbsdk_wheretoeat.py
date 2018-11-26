c8ebf60b162d5a273b7fc71d616e0afc1de0e02b
Mandl Cho <mandl.cho@icloud.com>
2018-11-26 17:38:28 +0800

    first implementation
    first pass of code basic functionality

---
+++ pyfbsdk_wheretoeat.py
@@ -0,0 +1,45 @@
+import pyfbsdk as fb
+import pyfbsdk_additions as fba
+
+import random as rand
+
+# define buttons
+btnGenerous = fb.FBButton()
+btnBroke = fb.FBButton()
+
+lFoodLocations = ["DailyCut",
+                  "Subway", 
+                  "Pho", 
+                  "iSteaks",
+                  "Carls Jr",
+                  "FoodBarn",
+                  "TwoChefs",
+                  "Red"]
+
+lCheapFoodLocations = ["Downstairs",
+                       "Guest La Mian",
+                       "Grass",
+                       "Bread",
+                       "Koufu",
+                       "FoodMaster"]
+
+def WhereToEat(pBudgetValue):
+    if pBudgetValue <= 10:
+        lCheapListCount = lCheapFoodLocations[:-1]
+        cheapnum = len(lCheapListCount)
+        cheapPlaceRandomIdx = rand.randint(0,cheapnum)
+        cheapLocation = lCheapFoodLocations[cheapPlaceRandomIdx]
+        # places that cost 10 or below
+        fb.FBMessageBox("Eat Cheap la", cheapLocation, "lets go")
+
+    if pBudgetValue >= 11:
+        # places that cost 11 or more
+        lExListCount = lFoodLocations[:-1]
+        exnum = len(lExListCount)
+        exPlaceRandomIdx = rand.randint(0,exnum)
+        exLocation = lFoodLocations[exPlaceRandomIdx]
+        fb.FBMessageBox("Feeling Generous eh?", exLocation, "lets go")
+
+        # pass
+
+WhereToEat(5)
