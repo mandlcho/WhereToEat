6846a51cac27c96833d23d7fc29c37b16c356fe8
Mandl Cho <mandl.cho@icloud.com>
2018-11-28 12:05:26 +0800

    implemented ui
    implemented PyQt ui via pyuic4
    added placeholder method to test functionality.

--- pyfbsdk_wheretoeat.py
+++ pyfbsdk_wheretoeat.py
@@ -5,15 +5,16 @@
 # Updated: 26 Nov 2018
 # =========================================================== #
 
+from PyQt4 import QtCore,QtGui
+import wheretoeatdesign
+import sys
+
 import random as rand
 import json
 
-budget = int(input("Tell me your budget in $!"))
+# budget = int(input("Tell me your budget in $!"))
 
-with open('locations.json')
-
-
-
+# data ======================================================= #
 
 lFoodLocations = ["DailyCut",
                   "Subway",
@@ -31,21 +32,39 @@ lCheapFoodLocations = ["Downstairs",
                        "Koufu",
                        "FoodMaster"]
 
-def WhereToEat(pBudgetValue):
-    if pBudgetValue <= 10:
-        # places that cost 10 or less
-        lCheapListCount = lCheapFoodLocations[:-1]
-        cheapnum = len(lCheapListCount)
-        cheapPlaceRandomIdx = rand.randint(0,cheapnum)
-        cheapLocation = lCheapFoodLocations[cheapPlaceRandomIdx]
-        print(cheapLocation)
+# =========================================================== #
 
-    if pBudgetValue >= 11:
-        # places that cost 11 or more
-        lExListCount = lFoodLocations[:-1]
-        exnum = len(lExListCount)
-        exPlaceRandomIdx = rand.randint(0,exnum)
-        exLocation = lFoodLocations[exPlaceRandomIdx]
-        print(exLocation)
+class WhereToEatApp(QtGui.QMainWindow, wheretoeatdesign.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(WhereToEatApp, self).__init__(parent)
+        self.setupUi(self)
+        self.btn_Tellme.clicked.connect(self.printHello)
 
-WhereToEat(budget)
+    def printHello(self):
+        print('code is working')
+
+    def WhereToEat(pBudgetValue):
+        if pBudgetValue <= 10:
+            # places that cost 10 or less
+            lCheapListCount = lCheapFoodLocations[:-1]
+            cheapnum = len(lCheapListCount)
+            cheapPlaceRandomIdx = rand.randint(0, cheapnum)
+            cheapLocation = lCheapFoodLocations[cheapPlaceRandomIdx]
+            print(cheapLocation)
+
+        if pBudgetValue >= 11:
+            # places that cost 11 or more
+            lExListCount = lFoodLocations[:-1]
+            exnum = len(lExListCount)
+            exPlaceRandomIdx = rand.randint(0, exnum)
+            exLocation = lFoodLocations[exPlaceRandomIdx]
+            print(exLocation)
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    form = WhereToEatApp()
+    form.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
---
+++ wheretoeatdesign.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'wheretoeat.ui'
+#
+# Created: Wed Nov 28 11:25:41 2018
+#      by: PyQt4 UI code generator 4.9.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(279, 190)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.gridLayout = QtGui.QGridLayout()
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.frame = QtGui.QFrame(self.centralwidget)
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName(_fromUtf8("frame"))
+        self.horizontalLayout = QtGui.QHBoxLayout(self.frame)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.btn_Tellme = QtGui.QPushButton(self.frame)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.btn_Tellme.sizePolicy().hasHeightForWidth())
+        self.btn_Tellme.setSizePolicy(sizePolicy)
+        self.btn_Tellme.setObjectName(_fromUtf8("btn_Tellme"))
+        self.verticalLayout_2.addWidget(self.btn_Tellme)
+        self.horizontalLayout.addLayout(self.verticalLayout_2)
+        self.gridLayout.addWidget(self.frame, 0, 0, 1, 1)
+        self.verticalLayout.addLayout(self.gridLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Where To Eat", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_Tellme.setText(QtGui.QApplication.translate("MainWindow", "Tell Me", None, QtGui.QApplication.UnicodeUTF8))
+
